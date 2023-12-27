6483c20296c7e23c81c5cd010969e83c9caea942
jonahleonmusic <154948112+jonahleonmusic@users.noreply.github.com>
2023-12-26 21:41:39 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,43 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+
+
+//songwritingCards section
+function newRhythym(length, genre){
+  let newRhythym = [{noteValue: .25, noteType: "normal"}, {noteValue: .125, noteType: "rest"}, {noteValue: .125, noteType: "normal"}, {noteValue: .5, noteType: "normal"}];
+
+  //check if valid rhythym is created
+  let noteCount = 0;
+  for (let i = 0; i < newRhythym.length; i++) {
+    noteCount += newRhythym[i].noteValue;
+  }
+
+  if noteCount % 1 = 0{ ///if note count is a whole integer (1 bar, 2 bars, 3 bars in length)
+    return noteCount;
+  } else {
+    console.log("invalid rhythym");
+  }
+  /////---------------------------
+  
+}
+function newCard (genre, key) {
+  let chordProgression = ["i", "IV", "V"];
+  let notes = [3,4,1];
+
+  //rhythym is a list of objects where each object is a single note, note can either be "normal", "dotted", or "rest"
+  let rhythym = newRhythym(1, genre);
+  let artwork = "inputArtImageURLHere";
+  let lyric = "crazy lyric!";
+  let voicing = "inputVoicingImageHere";
+  let newCard = {genre: genre, chordProgression: chordProgression, notes: notes, rhythym: rhythym, artwork: artwork, lyric: lyric, voicing: voicing};
+
+  return newCard;
+}
+
+
+
+var card1 = newCard("The Beatles");
+
+console.log(card1.genre);
